bfbb5398e95cc002d6a16d6af4861ceedfd096f8
Bozzay Gábor <111114673+GabbenPapa@users.noreply.github.com>
2025-05-18 15:01:56 +0200

    add map and form

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,13 +1,20 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
+  const navigate = useNavigate();
+
   const [serchParams, setSearchParams] = useSearchParams();
   const lat = serchParams.get("lat");
   const lng = serchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer}>
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
       <h1>Map Component</h1>
       <h2>
         Latitude: {lat} Longitude: {lng}
