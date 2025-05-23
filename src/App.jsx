b9924721e4dd2795f52870aecee81149c4f57ab8
Bozzay Gábor <111114673+GabbenPapa@users.noreply.github.com>
2025-05-23 16:24:13 +0200

    Add ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import Form from "./components/Form";
 import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 const API_URL = "http://localhost:8000";
 
@@ -26,7 +27,14 @@ function App() {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate to="cities" replace />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
